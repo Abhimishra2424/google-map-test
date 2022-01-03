e480f25649326f5b9bc34a0658dccd3a934d460d
Abhishek <abmishra700@gmail.com>
2022-01-03 18:18:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import GoogleMapReact from "google-map-react";
+import MyMarker from "./MyMarker";
+
+const points = [
+  { id: 1, title: "Round Pond", lat: 51.506, lng: -0.184 },
+  { id: 2, title: "The Long Water", lat: 51.508, lng: -0.175 },
+  { id: 3, title: "The Serpentine", lat: 51.505, lng: -0.164 },
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: "AIzaSyDiKc4HxX5G7EfneIZBN_Hlk2_luoT_yvo",
+          language: "en",
+          region: "US",
+        }}
+        defaultCenter={{ lat: 51.506, lng: -0.169 }}
+        defaultZoom={15}
+      >
+        {points.map(({ lat, lng, id, title }) => {
+          return (
+            <MyMarker key={id} lat={lat} lng={lng} text={id} tooltip={title} />
+          );
+        })}
+      </GoogleMapReact>
     </div>
   );
 }
-
-export default App;
